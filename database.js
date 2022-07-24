b70b1d0aa59937a1c10d62af6283587c259474cb
Sofi <sofiapazrodriguez@gmail.com>
2022-07-24 14:45:36 -0300

    funciona?

--- database.js
+++ database.js
@@ -1,108 +1,117 @@
-const express= require('express');
+
+const express = require('express');
 const app = express();
-const hbs = require('hbs');
 require('dotenv').config();
 const Port = process.env.PORT || 8080;
-const mysql2= require('mysql2');
+const hbs = require('hbs'); 
+const mysql = require('mysql2'); 
 const path = require('path');
-const { error } = require('console');
 
-const conexion = mysql2.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Holasofe09!",
-    port: 3306,
-    database: "astro",
+
+//Conectamos la app a una base de datos
+
+const conexion = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    port: process.env.PORTDB,
+    database: process.env.DATABASE,
 });
 
-const conectar = (
-    conexion.connect((error)=>{
-        if(error) throw error;
-        console.log('base de datos conectada!!!');
-    })
-    ); 
 
+
+const conectar = (
+conexion.connect((error) =>{
+      //  if(error) throw errorMonitor;
+        console.log('Base de Datos Conectada');
+})
+);
 
 
 app.use(express.json());
-
-app.use(express.static(path.join(__dirname,"public")));
-
-app.use(express.urlencoded({extended:false}));
-
-app.get('/', (req, res) => {
-    res.send('conectado a la base de datos');
-
-    });
-    app.get('/', (req, res) => {
-        res.render('index', {titulo:'Bienvenido a Pluton en linea'});
-
-    });
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.urlencoded({extended: false}));
 
 
-    app.get('/contacto', (req, res) => {
-        res.render('contacto', {titulo:'rellena el siguiente formulario'});
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
-    });
+//Rutas
 
-    //verbo post: para recibir datos, puede imprimirlo y/o enviarlo a la base de datos
-    app.post('/contacto', (req, res) => {
-        console.log(req.body);
+app.get('/', (req, res) =>{
+    res.render('index', {titulo: 'Bienvenido'})
+});
 
+app.get('/contacto', (req, res) =>{
+    res.render('contacto', {titulo: 'Envianos tu consulta'})
+});
 
+//Verbo http para recibir datos
+app.post('/contacto', (req,res) =>{
 
-        const {nombre, apellido, fechanac, email, motivo} = req.body;
+    //Desestructuracion
+
+    const{nombre, apellido, fechanac, email, motivo} = req.body;
+
+    console.log(nombre);
+    console.log(apellido);
+    console.log(fechanac);
+    console.log(correo);
+    console.log(motivo);
+
+    //validación básica
+    if(nombre == "" || fechanac == "" || email == ""){
+        let validacion = 'Te faltan uno o mas datos para continuar con la consulta'
+
+        res.render('contacto',  {
+            titulo: 'Envianos tu consulta',
+            validacion 
+        })
+    }else{
         console.log(nombre);
         console.log(apellido);
         console.log(fechanac);
-        console.log(email);
+        console.log(correo);
         console.log(motivo);
 
-     //validacion basica
+    res.send("¡Gracias! nos estaremos comunicando a la brevedad")
+    
+    //otra forma es creando funciones
 
-    if(nombre == "" || fechanac == "" || email == ""){
-        let validacion = 'Faltan datos para finalizar';
-
-        res.render('/contacto', {titulo:'envianos tu consulta'},
-        validacion
-        
-        )}else{
-            console.log(nombre, apellido, fechanac, email, motivo)
-            res.send('index', {titulo:'Gracias!'});
-        }
-
-         //conexion a la base de datos "astro"
-        let data = {
-            contacto_nombre:nombre,
-            contacto_apellido:apellido,
-            contacto_fechanac:fechanac,
-            contacto_email:email,
-            contacto_motivo:motivo
-        };
-        let sql= "INSERT INTO UTN SET ?"
-
-        let query = conexion.query (sql, data, (err, results) => {    
-            if(error) throw error;
+            //conectar()
             
-        //para que el usuario cargue datos y quede en la misma pagina:
-        //tuve que comentar 1***
-        res.render('contacto', {titulo:'rellena el formulario para tu consulta'})    
-        })
-    });
+            let data = {
+                nombre:nombre,
+                apellido:apellido,
+                fechanac:fechanac,
+                email:email,
+                motivo:motivo
+            };
 
-    app.listen(Port, ()=>{
-        console.log(`Servidor corriendo en el puerto ${Port}`);  
-    });
+            let sql = "INSERT INTO USUARIOS SET ?";
 
-    app.on('error', (error) => {
-        console.log(`Tenemos un error: ${error}`);  
-    }); 
+            let query = conexion.query (sql, data, (err, results) =>{
+            if(err) throw err;
+            res.render('contacto',  {titulo: 'Envianos tu consulta'})
+            });
+    }
+});
+//res.sendStatus(200).send('Tus datos han sido recibidos')
+//res.send('Tus datos han sido recibidos')
 
+app.get('/quienes-somos', (req, res) =>{
+    res.render('quienes-somos', {titulo: 'Animate a chusmear'})
+});
 
-    //configuramos la vista de la aplicacion
-    app.set('view engine', 'hbs');
-    app.set('views', path.join(__dirname,'views'));
-    console.log(__dirname)
-    //sacamos de npm: hay que crear carpeta partials para utilizarlo
-    hbs.registerPartials(path.join(__dirname, 'views/partials'));
+app.get('/index', (req, res) =>{
+    res.render('index', {titulo: 'Conocé las opciones que tenemos para vos'})
+});
 
+app.listen(Port, ()=>{
+    console.log(`Servidor corriendo en el Puerto ${Port}`);
+});
+
+app.on('error', (error) =>{
+    console.log(`Tenemos un error ${error}`);
+})
